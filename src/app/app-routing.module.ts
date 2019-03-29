4568fb35bf9e2ef170d37697e6d844a4590faaa6
who <who@whooami.me>
2019-03-29 19:31:33 +0000

    style: hide tutorial module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CheckTutorial } from './providers/check-tutorial.service';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/tutorial',
+	  redirectTo: '/app/tabs/about',
     pathMatch: 'full'
   },
   {
@@ -27,11 +26,6 @@ const routes: Routes = [
   {
     path: 'app',
     loadChildren: './pages/tabs-page/tabs-page.module#TabsModule'
-  },
-  {
-    path: 'tutorial',
-    loadChildren: './pages/tutorial/tutorial.module#TutorialModule',
-    canLoad: [CheckTutorial]
   }
 ];
 
